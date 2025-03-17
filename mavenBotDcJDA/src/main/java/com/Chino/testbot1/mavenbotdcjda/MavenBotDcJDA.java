843dfec79cfec5b3b85244c8d10f1860bc9f73eb
Kamil B. <97180792+ChinoIchino@users.noreply.github.com>
2025-03-17 18:35:34 +0100

    Update MavenBotDcJDA.java
    Added getWhenCreated that is used in most listeners so that there isnt a getWhenCreated function in each one.
    
    And some minor changes.

--- mavenBotDcJDA/src/main/java/com/Chino/testbot1/mavenbotdcjda/MavenBotDcJDA.java
+++ mavenBotDcJDA/src/main/java/com/Chino/testbot1/mavenbotdcjda/MavenBotDcJDA.java
@@ -1,11 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.Chino.testbot1.mavenbotdcjda;
 
 //import from java lib
 import java.io.IOException;
+import java.time.ZonedDateTime;
+
 import javax.security.auth.login.LoginException;
 
 //import the EventListener.java from listener file
@@ -18,6 +16,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 //import from JDA
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
@@ -28,7 +28,8 @@ public class MavenBotDcJDA {
 
     public MavenBotDcJDA() throws LoginException{
         //get the token in the .env file
-        config = Dotenv.configure().ignoreIfMissing().load();
+        //config = Dotenv.configure().ignoreIfMissing().load();
+        config = Dotenv.configure().load();
         String token = config.get("TOKEN");
         //create a shard named builder wit the token
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
@@ -36,11 +37,18 @@ public class MavenBotDcJDA {
         builder.setActivity(Activity.playing("On Chino Patience"));
         //open gateway to be able listen for messaage, guild join,... that are used in triggers
         builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS);
+        //let the bot retrieve all the users in the server (used in the initGetNameAndGetId function in eventMessageLogs)
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        //ask the bot to add everyone to the member cache
+        builder.setChunkingFilter(ChunkingFilter.ALL);
+        
         shardManager = builder.build();
+
         
         //shardManager.addEventListener(new EventListener(), new CommandsManager());
-        //add listeners
-        shardManager.addEventListener(new EventMessageLogs(),new EventRoleLogs());
+
+        //add listeners new EventMessageLogs(), new EventRoleLogs(), new EventChannelLogs(), new EventUserEntry()
+        shardManager.addEventListener(new EventMessageLogs(), new EventRoleLogs(), new EventChannelLogs(), new EventUserEntry());
     }
 
     //gets
@@ -51,6 +59,21 @@ public class MavenBotDcJDA {
         return config;
     }
 
+    public static String getWhenCreated(ZonedDateTime event){
+
+        if(event.getMinute() < 10){
+            return 
+            event.getDayOfMonth() + "/" + event.getMonthValue() + "/" + event.getYear() + " "
+            + event.getHour() + "h0" + event.getMinute() + " ("
+            + event.getOffset() + " " + event.getZone() + ")";
+        }
+        return 
+            event.getDayOfMonth() + "/" + event.getMonthValue() + "/" + event.getYear() + " "
+            + event.getHour() + "h" + event.getMinute() + " ("
+            + event.getOffset() + " " + event.getZone() + ")";
+    }
+
+
     public static void main(String[] args) throws IOException{
         //catching token with LoginException if token is wrong the bot isnt created and the "Error: ..." shows up
         try{
@@ -58,8 +81,6 @@ public class MavenBotDcJDA {
         } catch (LoginException e){
             System.out.println("Error: bot token is invalid!");
         }
-        
+
     }
-
-
 }
