5bfc57e032e94b3c859c9d1f0e0f6a4c60c303ab
Kamil B. <97180792+ChinoIchino@users.noreply.github.com>
2025-03-17 18:43:31 +0100

    Update EventRoleLogs.java
    Deleted the getEventTime, it use now getWhenCreated in the main file
    
    Minor changes

--- mavenBotDcJDA/src/main/java/com/Chino/testbot1/mavenbotdcjda/Listener/EventRoleLogs.java
+++ mavenBotDcJDA/src/main/java/com/Chino/testbot1/mavenbotdcjda/Listener/EventRoleLogs.java
@@ -1,29 +1,30 @@
 package com.Chino.testbot1.mavenbotdcjda.Listener;
 
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
+
+//the import as comments are for the initRoles
+//import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
 
+import com.Chino.testbot1.mavenbotdcjda.MavenBotDcJDA;
+
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleRemoveEvent;
 import net.dv8tion.jda.api.events.role.RoleCreateEvent;
 import net.dv8tion.jda.api.events.role.RoleDeleteEvent;
 import net.dv8tion.jda.api.events.role.update.RoleUpdateColorEvent;
 import net.dv8tion.jda.api.events.role.update.RoleUpdateNameEvent;
 import net.dv8tion.jda.api.events.role.update.RoleUpdatePermissionsEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+//import net.dv8tion.jda.api.utils.cache.SnowflakeCacheView;
 import net.dv8tion.jda.api.entities.Role;
 
-
-/*
-* Roles related
-*      GenericRoleUpdateUpdateEvent
-*          RoleUpdateMentionableEvent ? (need to see what is that)
-*          RoleUpdatePositionEvent ? (need to see what is that)
-*/
-
 public class EventRoleLogs extends ListenerAdapter{
+    private Role role;
     private String oldColor;
 
+    //for some reason when a role get his color modified and i call event.getOldColor() it always return null so i made a function to get and set the role color
     private String getColor(@NotNull RoleUpdateColorEvent event, boolean newColor){
         //if the boolean is false it send back the old color of the role
         if(newColor == false){
@@ -32,96 +33,109 @@ public class EventRoleLogs extends ListenerAdapter{
         //if the boolean is true it update the new value of the role color and send it back
         if(newColor == true){
             this.oldColor = event.getNewColor().getRed() + "," + event.getNewColor().getGreen() + "," + event.getNewColor().getBlue(); 
-            return event.getNewColor().getRed() + "," + event.getNewColor().getGreen() + "," + event.getNewColor().getBlue();
+            return this.oldColor;
         }
         return null;
     }
 
-    //I added the role as the input because the event differ based on the function
-    private String getEventTime(Role role){
-        ZonedDateTime roleAtTime = role.getTimeCreated().atZoneSameInstant(ZoneId.systemDefault());
-        //just to have a better display if the minute are under 10 so it display 14h03 and not 14h3
-        if(roleAtTime.getMinute() < 10){
-            return 
-            roleAtTime.getDayOfMonth() + "/" + roleAtTime.getMonthValue() + "/" + roleAtTime.getYear() + " "
-            + roleAtTime.getHour() + "h0" + roleAtTime.getMinute() + " ("
-            + roleAtTime.getOffset() + " " + roleAtTime.getZone() + ")";
-        }
-        return 
-            roleAtTime.getDayOfMonth() + "/" + roleAtTime.getMonthValue() + "/" + roleAtTime.getYear() + " "
-            + roleAtTime.getHour() + "h" + roleAtTime.getMinute() + " ("
-            + roleAtTime.getOffset() + " " + roleAtTime.getZone() + ")";
+    //the get/set are private, because for now i dont need to use it in other classes
+    //getRole is already used by jda so i called my get and set EventRole
+    private Role getEventRole(){
+        return this.role;
+    }
+    private void setEventRole(Role role){
+        this.role = role;
     }
 
+    //the init does not work for now :(
+    /*
+    public void initRoles(SnowflakeCacheView<Role> cache){
+        List<Role> temp = cache.asList();
+        
+        for(int i = 0; i < temp.size(); i++){
+            //System.out.println(temp.get(i));
+            setEventRole((Role) temp.get(i));
+            //System.out.println(i +" : GetRole = " + getRole());
+        } 
+    }
+    */
+
     //Events Listener for role creation/modification and erasing
-    //get activated when a role is created
     @Override
     public void onRoleCreate(@NotNull RoleCreateEvent event){
-        //get the role who activated the event
-        Role role = event.getRole();
+        setEventRole(event.getRole());
 
         event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
-            "The role: " + role.getName() + " have been created! \nAt: " + getEventTime(role) 
-            + "\nColor: Default Role Color" + "\nId: " + role.getId()
+            //event.getMessage().getTimeCreated().atZoneSameInstant(ZoneId.systemDefault()
+            "A new role have been created! \nAt: " + MavenBotDcJDA.getWhenCreated(event.getRole().getTimeCreated().atZoneSameInstant(ZoneId.systemDefault()))
+            + "\nId: " + event.getRole().getId()
             ).queue();
     }
 
     @Override
     public void onRoleDelete(@NotNull RoleDeleteEvent event){
-        Role role = event.getRole();
 
         event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
-            "The role: " + role.getName() + " have been deleted! \nAt: " + getEventTime(role) + "\nId: " + role.getId()
+            "The role \"" + event.getRole().getName() + "\" have been deleted!" 
+            + "\nId: " + event.getRole().getId()
             ).queue();
     }
 
     @Override
     public void onRoleUpdatePermissions(@NotNull RoleUpdatePermissionsEvent event){
-        Role role = event.getRole();
         String newPerms = event.getNewPermissions().toString();
 
         event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
-            "The role: " + role.getName() + " got his perms modified!"+ "\nAt: " + getEventTime(role)
-            + "\nOld perms: " + event.getOldValue() + "\nNew perms: " + newPerms 
+            "The role: \"" + event.getRole().getName() + "\" got his perms modified!"
+            + "\nOld perms: " + event.getOldValue() 
+            + "\nNew perms: " + newPerms 
             + "\nId: " + role.getId()
             ).queue();
     }
 
     @Override
     public void onRoleUpdateName(@NotNull RoleUpdateNameEvent event){
-        Role role = event.getRole();
 
         event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
-            "The role: " + event.getOldValue() + " got his name modified!" + "\nAt: " + getEventTime(role)
-            + "\nOld name: " + event.getOldValue() + "\nNew name: " + event.getNewValue()
-            + "\nId: " + role.getId()
+            "The role: \"" + event.getOldValue() + "\" got his name modified!" 
+            + "\nNew name: " + event.getNewValue()
+            + "\nId: " + event.getRole().getId()
             ).queue();
     }
 
     @Override
     public void onRoleUpdateColor(@NotNull RoleUpdateColorEvent event){
-        Role role = event.getRole();
 
         if(this.oldColor != null){
             event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
-                "The role: " + role.getName() + " got his color modified! \nAt: " + getEventTime(role) 
+                "The role: " + role.getName() + " got his color modified!"
                 + "\nOld color: " + getColor(event, false) + "\nNew color: " + getColor(event, true)
                 + "\nId: " + role.getId()
             ).queue();
         }
         else if(this.oldColor == null){
             event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
-                "The role: " + role.getName() + " got his color modified! \nAt: " + getEventTime(role) 
+                "The role: " + role.getName() + " got his color modified!"
                 + "\nOld color: Default Role Color"
                 + "\nNew color: " + getColor(event, true)
                 + "\nId: " + role.getId()
             ).queue();
         }
-        else{
-            event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
-                "Error: this.oldColor isnt null nor does it have a value"
-            ).queue();
-        }
     }
-    
+
+    @Override
+    public void onGuildMemberRoleAdd(GuildMemberRoleAddEvent event) {
+
+        event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
+        "The role: \"" + getEventRole().getName() + "\" have been added to the user: " + event.getUser().getName()
+        ).queue();
+    }
+
+    @Override
+    public void onGuildMemberRoleRemove(GuildMemberRoleRemoveEvent event) {
+
+        event.getGuild().getTextChannelsByName("message_logs", true).get(0).sendMessage(
+            "The role: \"" + getEventRole().getName() + "\" have been removed from the user: " + event.getUser().getName()
+            ).queue();
+    }
 }
